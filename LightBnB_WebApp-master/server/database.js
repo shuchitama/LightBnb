3909522034f4580b846cab858d7e9cfe7cc5c923
Shuchita Mahey <shuchitamahey@Shuchitas-MacBook-Pro.local>
2020-03-05 11:54:05 -0800

    connect getAllReservations to lightbnb db

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -70,7 +70,21 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = `
+  SELECT properties.*, reservations.*, AVG(property_reviews.rating) AS average_rating
+  FROM properties
+  JOIN reservations ON reservations.property_id = properties.id
+  JOIN property_reviews ON property_reviews.property_id = properties.id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id 
+  HAVING end_date < now()::date
+  ORDER BY start_date DESC
+  LIMIT 10;
+  `;
+  const values = [guest_id];
+
+  return pool.query(query, values)
+  .then(res => res.rows)
 }
 exports.getAllReservations = getAllReservations;
 
